531a598dcd0155d3e8ea6b7ce53b0720f7cf0314
tonynagyeurope <tony.nagy.europe@gmail.com>
2025-02-16 20:41:46 +0100

    Adding the transferFrom and approve methods to the ERC-20 interface

--- contracts/libs/TokenUtils.sol
+++ contracts/libs/TokenUtils.sol
@@ -33,7 +33,10 @@ interface IERC20 {
     function transfer(address recipient, uint256 amount) external returns (bool);
     function balanceOf(address account) external view returns (uint256);
     function totalSupply() external view returns (uint256);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
 }
+
 /// @notice ERC-1363 interface
 interface IERC1363 is IERC20 {
     function transferAndCall(address to, uint256 value) external returns (bool);
